d61c14728e966ab36056a8d769da9cb4e65720ab
phebonacci <phebesawan@gmail.com>
2021-05-04 14:49:57 +0800

    Fix prebuilding

---
+++ prebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Starting prebuild process"
+
+USER_SET_BRANCH=$1
+DETECTED_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+BRANCH="${USER_SET_BRANCH:-$DETECTED_BRANCH}"
+
+# Set branch on which the CMS edits files by creating
+# the netlify config from the cms-config and overwrite
+# the branch setting.
+yes | cp -rf ./cms-config.yml ./public/admin/config.yml
+sed -i -e "s|CMS_BRANCH|$BRANCH|g" ./public/admin/config.yml
